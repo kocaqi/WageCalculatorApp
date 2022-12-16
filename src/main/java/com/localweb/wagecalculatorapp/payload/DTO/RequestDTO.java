6e336d209dd7f07936d1fc5ad0cbe6a32c14af2b
User <kkocaqi20@epoka.edu.al>
2022-12-16 16:44:54 +0100

    Better Swagger Configs

--- src/main/java/com/localweb/wagecalculatorapp/payload/DTO/RequestDTO.java
+++ src/main/java/com/localweb/wagecalculatorapp/payload/DTO/RequestDTO.java
@@ -2,11 +2,15 @@ package com.localweb.wagecalculatorapp.payload.DTO;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @Data
 public class RequestDTO {
+    @NotEmpty(message = "This Field cannot be null!")
     private String keyword;
+    @NotEmpty(message = "This Field cannot be null!")
     private LocalDate dateFrom;
+    @NotEmpty(message = "This Field cannot be null!")
     private LocalDate dateTo;
 }
